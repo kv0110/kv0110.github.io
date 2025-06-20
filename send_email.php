444a9e6b00cea7179a7b597facc10cbd665325f9
kavyamridula venkatesan <kavyamridula@gmail.com>
2025-06-20 14:31:33 -0700

    test 2 changed link

--- send_email.php
+++ send_email.php
@@ -3,7 +3,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['msg']);
     $to = "kavyamridula@gmail.com"; 
     $subject = "New Chat Message";
-    $headers = "From: file:///Users/kavyamridulavenkatesan/Github/passproj/kv0110.github.io/resource_table.html"; // replace with real git link
+    $headers = "From: https://kv0110.github.io/resource_table.html"; // replace with real git link
     $body = "You received a new message:\n\n" . $message;
 
     if (mail($to, $subject, $body, $headers)) {
